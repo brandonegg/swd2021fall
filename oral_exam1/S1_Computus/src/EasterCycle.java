4d1a7d409219f91c0eb3a9958d5952bd2b1d39a6
beggr <brandon-egger@uiowa.edu>
2021-09-08 13:04:41 -0500

    Updated outputEasterDateCounts() to output in order

--- oral_exam1/S1_Computus/src/EasterCycle.java
+++ oral_exam1/S1_Computus/src/EasterCycle.java
@@ -1,9 +1,21 @@
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.TreeMap;
+import java.util.SortedMap;
 
 public class EasterCycle {
         public static  Map<String, Integer> countEasterDates() {
-            Map<String, Integer> dateOccurenceCounters =  new TreeMap<String, Integer>();
+            Map<String, Integer> dateOccurenceCounters =  new LinkedHashMap<>();
+
+            //The following ensures dates are stored in order
+            //Generate march days:
+            for (int i = 22; i<=31; i++) {
+                dateOccurenceCounters.put(Integer.toString(i)+ " March", 0);
+            }
+            //Generate april days:
+            for (int i = 1; i<=25; i++) {
+                dateOccurenceCounters.put(Integer.toString(i)+ " April", 0);
+            }
 
             for (int i = 1; i<=5700000; i++) {
                 Easter easterDate = new Easter(i);
