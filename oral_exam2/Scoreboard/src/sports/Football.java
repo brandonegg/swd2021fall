7eb8ec0eef69d66c3cb63855baf87dffa804d21a
beggr <brandon-egger@uiowa.edu>
2021-10-18 14:12:48 -0500

    Fixed weird main class not found

--- oral_exam2/Scoreboard/src/sports/Football.java
+++ oral_exam2/Scoreboard/src/sports/Football.java
@@ -1,11 +1,12 @@
 package sports;
 
 import dataholder.Team;
+import enums.Sport;
 
 public class Football extends Game {
 
     public Football(Team home, Team away) {
-        super();
+        super(home, away, Sport.FOOTBALL);
     }
 
 }
