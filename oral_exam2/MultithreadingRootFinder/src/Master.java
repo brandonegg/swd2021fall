167cd19e7901352c807820cb2a6dc2957ea8fec0
beggr <brandon-egger@uiowa.edu>
2021-10-25 13:10:37 -0500

    fix

--- oral_exam2/MultithreadingRootFinder/src/Master.java
+++ oral_exam2/MultithreadingRootFinder/src/Master.java
@@ -1,2 +1,17 @@
 public class Master {
+
+    public static void main(String[] args) {
+        double[] roots = findRoots(1,2,3);
+
+        System.out.println(roots[0] + " " + roots[1]);
+    }
+
+    public static double[] findRoots(double a, double b, double c) {
+        // x= (-b +- sqrt(b^2 -4ac))/2a
+        double root1 = (-b + Math.sqrt(Math.pow(b,2) - 4*a*c))/(2*a);
+        double root2 = (-b - Math.sqrt(Math.pow(b,2) - 4*a*c))/(2*a);
+
+        return new double[] {root1, root2};
+    }
+
 }
--- oral_exam2/MultithreadingRootFinder/src/RootFinder.java
+++ oral_exam2/MultithreadingRootFinder/src/RootFinder.java
@@ -2,7 +2,13 @@ public class RootFinder implements Runnable {
 
     @Override
     public void run() {
-        
+
+    }
+
+    private void findRoots(double a, double b, double c) {
+        // x= (-b +- sqrt(b^2 -4ac))/2a
+        double root1 = (-b + Math.sqrt(Math.pow(b,2) - 4*a*c))/(2*a);
+        double root2 = (-b - Math.sqrt(Math.pow(b,2) - 4*a*c))/(2*a);
     }
 
 }
