f36370a57bd8092ae279932e775ec8690fad9a64
Brandon E <beggr@uiowa.edu>
2021-10-25 13:15:21 -0500

    First test passed

--- oral_exam2/MultithreadingRootFinder/src/Master.java
+++ oral_exam2/MultithreadingRootFinder/src/Master.java
@@ -1,11 +1,12 @@
 public class Master {
 
     public static void main(String[] args) {
-        double[] roots = findRoots(1,2,3);
+        double[] roots = findRoots(1,2,-3);
 
         System.out.println(roots[0] + " " + roots[1]);
     }
 
+    //TODO: Doesn't work for imaginaries!
     public static double[] findRoots(double a, double b, double c) {
         // x= (-b +- sqrt(b^2 -4ac))/2a
         double root1 = (-b + Math.sqrt(Math.pow(b,2) - 4*a*c))/(2*a);
