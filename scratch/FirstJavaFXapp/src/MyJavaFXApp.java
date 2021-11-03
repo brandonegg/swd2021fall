0e137bf029d7aeafd93140bc3852a0a51b2173f6
Brandon P Egger <beggr@engr.uiowa.edu>
2021-11-02 20:13:08 -0500

    added javafx example

---
+++ scratch/FirstJavaFXapp/src/MyJavaFXApp.java
@@ -0,0 +1,29 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MyJavaFXApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("MyJavaFXApp.fxml"));
+
+            Scene scene = new Scene(root);
+
+            primaryStage.setTitle("My Application");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
