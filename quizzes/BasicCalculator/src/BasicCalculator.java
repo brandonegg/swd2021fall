9b4ebdd86185bfbecfcc0065ef32bc2630dcd3ea
Brandon P Egger <beggr@engr.uiowa.edu>
2021-11-04 10:46:28 -0500

    11/4/2021 basic calculator

--- quizzes/BasicCalculator/src/BasicCalculator.java
+++ quizzes/BasicCalculator/src/BasicCalculator.java
@@ -1,2 +1,29 @@
-public class BasicCalculator {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class BasicCalculator extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("BasicCalculator.fxml"));
+
+            Scene scene = new Scene(root);
+
+            primaryStage.setTitle("Calculator");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
