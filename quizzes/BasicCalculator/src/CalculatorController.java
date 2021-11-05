950eb1d301221e5fa49fb42a40e8e371a635a75c
Brandon P Egger <beggr@engr.uiowa.edu>
2021-11-04 22:50:49 -0500

    11/4/2021 in class quiz

--- quizzes/BasicCalculator/src/CalculatorController.java
+++ quizzes/BasicCalculator/src/CalculatorController.java
@@ -1,2 +1,38 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class CalculatorController {
+
+    CalculatorModel calculatorModel;
+
+    public CalculatorController() {
+        calculatorModel = new CalculatorModel();
+    }
+
+    @FXML
+    private Label displayField;
+
+    @FXML
+    void submitClearPress(ActionEvent event) {
+
+    }
+
+    @FXML
+    void submitNumberPress(ActionEvent event) {
+        calculatorModel.addCurrentInt(((Button)event.getSource()).getText());
+        updateDisplayField();
+    }
+
+    public void updateDisplayField() {
+        displayField.setText(Integer.toString(calculatorModel.getCurrentNumber()));
+    }
 }
+
+/*
+submitNumberPress()
+submitOperatorPress()
+
+displayField
+ */
--- quizzes/BasicCalculator/src/CalculatorModel.java
+++ quizzes/BasicCalculator/src/CalculatorModel.java
@@ -1,4 +1,49 @@
 public class CalculatorModel {
-}
 
-//Add multiply and add methods inside alongside method for storing data
+    private int previousNumber;
+    private int currentNumber;
+    private char operator;
+
+    public CalculatorModel() {
+        previousNumber = 0;
+        currentNumber = 0;
+        operator = '0';
+    }
+
+    public void performComputation() {
+        switch(operator){
+            case '+':   currentNumber = previousNumber+currentNumber;
+                        break;
+            case '-':   currentNumber = previousNumber-currentNumber;
+                        break;
+            case '*':   currentNumber = previousNumber*currentNumber;
+                        break;
+            case '/':   currentNumber = previousNumber/currentNumber;
+            default:    break;
+        }
+        operator = '0';
+    }
+
+    public void setOperator(String letter) {
+        operator = letter.charAt(0);
+    }
+
+    public void addCurrentInt(String number) {
+        currentNumber = Integer.parseInt(Integer.toString(currentNumber)+number);
+    }
+
+    public void clear() {
+        previousNumber = 0;
+        currentNumber = 0;
+    }
+
+    public void pushBackCurrentNumber() {
+        previousNumber = currentNumber;
+        currentNumber = 0;
+    }
+
+    public int getCurrentNumber() {
+        return currentNumber;
+    }
+
+}
